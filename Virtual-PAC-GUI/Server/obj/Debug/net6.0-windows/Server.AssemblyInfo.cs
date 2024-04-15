d6e514d97caa1fc3f1c629b0d1563a8c4fd2250e
Alailton <alailtonjunior@usp.br>
2024-04-15 11:58:18 -0300

    locally

--- Virtual-PAC-GUI/Server/obj/Debug/net6.0-windows/Server.AssemblyInfo.cs
+++ Virtual-PAC-GUI/Server/obj/Debug/net6.0-windows/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bccc000c8d2640ee5b25737d222fe3a719bcf15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8b496a3849dd9c7f5d29c7bd7184e642bc56a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
